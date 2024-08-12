a0231566541c0fd9bf33d032cde8a7c1ecdc8348
Pavel Lyubich <pavel@lyubich.com>
2024-08-12 18:13:28 +0300

    updating copyright

--- plugins-management/installer/db/seed.sql
+++ plugins-management/installer/db/seed.sql
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2024 Paladin Cloud, Inc. or its affiliates. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy
+ * of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
 USE
     `pacmandata`;
 
@@ -50,4 +66,5 @@ INSERT INTO `plugin_policy_definitions` (source, target_name, cwe_enabled, cve_e
 VALUES ('crowdstrike', 'vminstance', _binary '', _binary '', _binary '', 'instanceId', '_resourceid', 'system', CURDATE());
 INSERT INTO `plugin_policy_definitions` (source, target_name, cwe_enabled, cve_enabled, mitre_enabled, asset_lookup_key, source_asset_key, created_by, created_date)
 VALUES ('crowdstrike', 'workstation', _binary '', _binary '', _binary '', 'externalId', '_resourceid', 'system', CURDATE());
+
 ########## end of plugin_policy_definitions seed data ##########
