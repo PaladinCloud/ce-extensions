12492516727b163380a0f29bcfc084693c5130ea
Pavel Lyubich <pavel@lyubich.com>
2024-10-14 15:02:28 -0700

    [NEXT-244] refactoring

--- ui-asset-management/api/svc-asset-details/extension/clients/secrets_client_test.go
+++ ui-asset-management/api/svc-asset-details/extension/clients/secrets_client_test.go
@@ -18,6 +18,7 @@ package clients
 
 import (
 	"context"
+	"os"
 	"testing"
 )
 
@@ -25,21 +26,11 @@ func TestNewSecretsClient(t *testing.T) {
 	t.Run("NewSecretsClient", func(t *testing.T) {
 		ctx := context.Background()
 
-import "testing"
-import "os"
-
-func TestNewSecretsClient(t *testing.T) {
-	t.Run("NewSecretsClient", func(t *testing.T) {
 		assumeRole := os.Getenv("AWS_ASSUME_ROLE_ARN")
 		if assumeRole == "" {
 			t.Fatal("AWS_ASSUME_ROLE_ARN environment variable not set")
 		}
-		_, err := NewSecretsClient(assumeRole, "us-east-1")
-		if err != nil {
-			t.Errorf("Error loading AWS config: %v", err)
-		}
-	})
-}
+
 		_, err := NewSecretsClient(ctx, assumeRole, "us-east-1")
 		if err != nil {
 			t.Errorf("Error loading AWS config: %v", err)
