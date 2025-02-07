13980fa7d6d8d1b27d05de781fbc39549a4de030
Kevin Tavog <165935671+kevin-paladin@users.noreply.github.com>
2025-02-07 09:15:28 -0800

    Both the delta engine and asset state service handle both SQS & SNS messages

--- assets-management/services/svc-asset-state/src/main/java/com/paladincloud/assetstate/AssetStateDoneHandler.java
+++ assets-management/services/svc-asset-state/src/main/java/com/paladincloud/assetstate/AssetStateDoneHandler.java
@@ -3,7 +3,9 @@ package com.paladincloud.assetstate;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.SQSEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -17,7 +19,22 @@ public class AssetStateDoneHandler implements RequestHandler<SQSEvent, Integer> 
         var mapper = new ObjectMapper();
         for (var message : event.getRecords()) {
             try {
-                var startMessage = mapper.readValue(message.getBody(), StartMessage.class);
+                var body = message.getBody();
+                if (body.contains("\"Message\":")) {
+                    try {
+                        var result = new ObjectMapper().readValue(body, HashMap.class);
+                        body = result.get("Message").toString();
+                        if (body == null) {
+                            LOGGER.error("Unable to find 'Message' in JSON body: {}", body);
+                            continue;
+                        }
+                    }  catch (JsonProcessingException e) {
+                        LOGGER.error("Unable to parse SNS message", e);
+                        continue;
+                    }
+                }
+
+                var startMessage = mapper.readValue(body, StartMessage.class);
                 componentResolver.buildAssetStateJob().run("AssetState", startMessage);
             } catch (Exception e) {
                 LOGGER.error("error occurred in AssetState service", e);
